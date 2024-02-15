8b819fb04182badd9ec873d83961f0a2d9014462
Caique <caiquenaimi.564@gmail.com>
2024-02-14 21:55:11 -0300

    Add ScrollView and update content in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 import Carousel from "react-native-snap-carousel";
 
 export default function App() {
@@ -40,51 +40,53 @@ export default function App() {
 
   return (
     <View className="flex-1 bg-black items-center justify-center">
-      <View className="p-4 bg-black">
-        <View>
-          <Text className="text-center text-xl text-white">Caique Naimi</Text>
-          <Image
-            className="rounded-full h-64 w-64 mx-auto"
-            source={require("./assets/imagens/caiquenaimi.png")}
-          />
-          <Text className="text-center text-xl text-white">
-            Aluno de Desenvolvimento de Sistemas cursando o 3º Semestre
-          </Text>
-          <Text className="text-center text-xl text-white mt-4">
-            Sobre mim:
-          </Text>
-          
-          <Text className="text-center text-xl text-white mt-4">Projetos:</Text>
-          <View className="flex-1 items-center justify-center">
-            <Carousel
-              data={projects}
-              renderItem={({ item }) => (
-                <View className="mb-4">
-                  <Text
-                    style={{ color: colors[item.color] }}
-                    className={`text-center text-xl text-white mt-4`}	
-                  >
-                    {item.title}
-                  </Text>
-                  <Image
-                    style={{ width: "100%", height: 237, borderRadius: 10 }}
-                    source={item.img}
-                  />
-                  <Text className="text-center text-base text-white mt-1">
-                    {item.description}
-                  </Text>
-                  <Text className="text-center text-base text-white mt-1">
-                    Tecnologias: {item.technologies.join(", ")}
-                  </Text>
-                </View>
-              )}
-              sliderWidth={200}
-              itemWidth={200}
-              windowSize={3}
-            />
-          </View>
-        </View>
+  <View className="p-4 bg-black">
+    <View>
+      <Text className="text-center text-xl text-white font-bold">Caique Naimi</Text>
+      <Image
+        className="rounded-full h-60 w-64 mx-auto"
+        source={require("./assets/imagens/caiquenaimi.png")}
+      />
+      <Text className="text-center text-l text-white mt-4">
+        Aluno de Desenvolvimento de Sistemas cursando o 3º Semestre
+      </Text>
+      <Text className="text-center text-l text-white mt-4 font-semibold">
+        Um entusiasta da programação em constante busca por aprendizado.
+        Sempre empenhado em criar soluções inovadoras.
+      </Text>
+
+      <Text className="text-center text-xl text-white mt-4 font-bold">Projetos:</Text>
+      <View className="flex-1 items-center justify-center">
+        <Carousel
+          data={projects}
+          renderItem={({ item }) => (
+            <View className="mb-4">
+              <Text
+                style={{ color: colors[item.color] }}
+                className={`text-center text-l text-white mt-4 font-semibold`}
+              >
+                {item.title}
+              </Text>
+              <Image
+                style={{ width: "100%", height: 237, borderRadius: 10 }}
+                source={item.img}
+              />
+              <Text className="text-center text-base text-white mt-1">
+                {item.description}
+              </Text>
+              <Text className="text-center text-base text-white mt-1">
+                Tecnologias: {item.technologies.join(", ")}
+              </Text>
+            </View>
+          )}
+          sliderWidth={200}
+          itemWidth={200}
+          windowSize={3}
+        />
       </View>
     </View>
+  </View>
+</View>
+
   );
 }
